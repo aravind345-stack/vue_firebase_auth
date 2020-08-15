5dedf507e4161c113ca8d20b5a78859c2bca3bd6
Neeharika Surampudi <neeharika.surampudi1995@gmail.com>
2020-08-15 17:06:05 +0530

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="vue-tempalte">
+    <!-- Navigation -->
+    <nav class="navbar shadow bg-white rounded justify-content-between flex-nowrap flex-row fixed-top">
+      <div class="container">
+        <a class="navbar-brand float-left" href="/" target="_blank">
+          Basic Auth Site
+        </a>
+        <ul class="nav navbar-nav flex-row float-right">
+          <li class="nav-item">
+            <router-link class="nav-link pr-3" to="/login">Sign in</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="btn btn-outline-primary" to="/">Sign up</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <!-- Main -->
+    <div class="App">
+      <div class="vertical-center">
+        <div class="inner-block">
+          <router-view />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import * as firebase from 'firebase';
+import router from './router'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@/assets/css/main.css'
 
 Vue.config.productionTip = false
 
+var firebaseConfig = {
+  apiKey: "AIzaSyBJHM3tQpc4JoJUeBRK5W5WOaxCLXrtFrI",
+  authDomain: "basicauth-71605.firebaseapp.com",
+  databaseURL: "https://basicauth-71605.firebaseio.com",
+  projectId: "basicauth-71605",
+  storageBucket: "basicauth-71605.appspot.com",
+  messagingSenderId: "423653127949",
+  appId: "1:423653127949:web:4f192c69e98958c7f72244",
+  measurementId: "G-KVZR1V11R1"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
